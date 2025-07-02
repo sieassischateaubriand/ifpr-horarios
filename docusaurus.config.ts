aff5df8d3121d88cdfd53e6f719bb96e7f19da5d
sieassischateaubriand <sie.assischateaubriand@ifpr.edu.br>
2025-07-02 18:19:21 -0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,8 @@ const config: Config = {
   url: 'https://sieassischateaubriand.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ifpr-horarios/',
+//  baseUrl: '/ifpr-horarios/',
+baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
